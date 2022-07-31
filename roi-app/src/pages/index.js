fc8d658f66c0785c63d522949894278e89e54abc
Bledi2002 <100237301+Bledi2002@users.noreply.github.com>
2022-07-31 02:01:33 +0200

    create home

--- roi-app/src/pages/index.js
+++ roi-app/src/pages/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import './index.scss';
 import foto1 from '../foto/foto1.png';
+import foto2 from '../foto/pakofull.png';
+import foto3 from '../foto/pakopremi.png';
+import foto4 from '../foto/pakolight.jpeg';
+import artikull1 from '../foto/artikulli1.jpeg';
+
+
 
 const Home = () => {
   return (
-    <div className="foto">
-         <img src={foto1}></img>
-    </div>
+    <>
+      <div className="foto1">
+        <img src={foto1}></img>
+      </div>
+      <div className="fotot">
+
+        <img src={foto2}></img>
+        <img src={foto3}></img>
+        <img src={foto4}></img>
+      </div>
+        <div className="lajmet">
+        <h1>Lajmet e Fundit</h1>
+        </div>
+        <div className="lajmet1">
+        <div className="artikulli1">
+        <img src={artikull1}></img>
+        </div>
+        </div>
+        <div className="lajmet1">
+        <div className="artikulli1">
+        <img src={artikull1}></img>
+        </div>
+        </div>
+        <div className="lajmet1">
+        <div className="artikulli1">
+        <img src={artikull1}></img>
+        </div>
+        </div>
+    </>
   );
 };
 
