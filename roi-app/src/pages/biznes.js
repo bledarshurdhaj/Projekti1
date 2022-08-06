0b4ea154e5f633be5ca58923a2390115cc0a88bb
Bledi2002 <100237301+Bledi2002@users.noreply.github.com>
2022-08-06 03:06:33 +0200

    Update Slider

--- roi-app/src/pages/biznes.js
+++ roi-app/src/pages/biznes.js
@@ -1,12 +1,13 @@
-// import Swiper core and required modules
+
 import React from 'react';
 import './biznes.scss';
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import Footer from '../components/footer';
-import foto23 from '../foto/pako23.png';
+import pako1 from '../foto/pakoo.png';
+import pako2 from '../foto/foto400.png';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -17,26 +18,25 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 const data = [
     {
         id: 1,
-        title: 'PAKO CALL  & GAMING CENTERS',
-        description: 'Përjeto pasionin unik me platformën tonë. Me më shumë se 200 kanale me super kualitet transmetimi',
-
+        title: 'PAKO  GAMING CENTERS',
+        description: 'Internet të dedikuar me shpejtësi të ndryshme , Shërbime të kombinuara, Internet dhe TV kabllor,Përkrahje teknike 24h',
     },
     {
         id: 2,
         title: 'PAKO  HOTELE',
-        description: 'Përjeto pasionin unik me platformën tonë. Me më shumë se 250 kanale me super kualitet transmetimi',
+        description: 'Internet dhe DTV për çdo dhomë , Wi - Fi routera , Shërbim Teknik 24h , Zgjedhje të specializuara , Personel të dedikuar',
 
     },
     {
         id: 3,
         title: 'PAKO HORECA',
-        description: 'Përjeto pasionin unik me platformën tonë. Me më shumë se 300 kanale me super kualitet transmetimi',
+        description: 'Internet dhe DTV , Wi - Fi router për çdo lokal , VPN sipas kërkesës tuaj,Shërbim Teknik 24h',
 
     },
     {
         id: 4,
         title: 'PAKO OSME',
-        description: 'Përjeto pasionin unik me platformën tonë. Me më shumë se 300 kanale me super kualitet transmetimi',
+        description: 'Internet të dedikuar, Internet + DTV , VPN sipas kërkesës tuaj ,Ip Reale ,24h Përkrahje Teknike',
 
     },
 ]
@@ -44,39 +44,42 @@ const data = [
 function Biznes() {
     return (
         <>
-<div className="first">
-    <h1 className="font">PAKOT PER BIZNESE</h1>
-    <div className="cards">
-        <Swiper
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            spaceBetween={50}
-            slidesPerView={3}
-            navigation
-            pagination={{ clickable: true }}
-            scrollbar={{ draggable: true }}
-            onSwiper={(swiper) => console.log(swiper)}
-            onSlideChange={() => console.log('slide change')}
-        >
-            {data.map(pako => (
-                <SwiperSlide key={pako.id} className="slide">
-                    <div className="slide-kontenti">
-                        <div className="pako.image">
-                            <img src={foto23} alt='' className="user-photo"></img>
-                        </div>
-                        <h5>{pako.title}
-                        </h5>
-                        <p className="pako-decription"> " <i>{pako.description}</i>"</p>
-                    </div>
-                </SwiperSlide>
-            ))}
-        </Swiper>
+  <div className="eee">
+  <img src={pako2}></img>
+  </div>
+            <div className="first">
+     
+                <h1 className="font">PAKOT PER BIZNESE</h1>
+                <div className="cards">
+                    <Swiper
+                        modules={[Navigation, Pagination, Scrollbar, A11y]}
+                        spaceBetween={50}
+                        slidesPerView={2}
+                        navigation
+                        pagination={{ clickable: true }}
+                        scrollbar={{ draggable: true }}
+                        onSwiper={(swiper) => console.log(swiper)}
+                        onSlideChange={() => console.log('slide change')}
+                    >
+                        {data.map(pako => (
+                            <SwiperSlide key={pako.id} className="slide">
+                                <div className="slide-kontenti">
+                                    <div className="pako-image">
+                                        <img src={pako1}></img>
+                                    </div>
+                                    <h3 className="h3">{pako.title} </h3>
+                                    <p className="pako-decription">{pako.description}</p>
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
 
-        </div>
-        </div>
-        <div>
-            <Footer/>
-        </div>
-</>
+                </div>
+            </div>
+            <div>
+                <Footer />
+            </div>
+        </>
     );
 };
 export default Biznes;
